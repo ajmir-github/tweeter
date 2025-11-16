9907a6353f1d51395fb039ff2fa8c77aff65877c
Ajmir Raziqi <ajmir.ng3@gmail.com>
2025-11-16 17:47:36 +0200

    fix prisma path

--- server/src/model/prisma/internal/class.ts
+++ server/src/model/prisma/internal/class.ts
@@ -34,6 +34,10 @@ const config: runtime.GetPrismaClientConfig = {
         "fromEnvVar": null,
         "value": "windows",
         "native": true
+      },
+      {
+        "fromEnvVar": null,
+        "value": "rhel-openssl-3.0.x"
       }
     ],
     "previewFeatures": [],
@@ -47,17 +51,16 @@ const config: runtime.GetPrismaClientConfig = {
     "db"
   ],
   "activeProvider": "sqlite",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": null,
-        "value": "file:../dev.db"
+        "value": "file:dev.db"
       }
     }
   },
-  "inlineSchema": "// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\ngenerator client {\n  provider = \"prisma-client\"\n  output   = \"../src/model/prisma\"\n}\n\ndatasource db {\n  provider = \"sqlite\"\n  // url      = env(\"DATABASE_URL\")\n  url      = \"file:../dev.db\"\n}\n\nmodel Profile {\n  id   Int     @id @default(autoincrement())\n  name String?\n}\n",
-  "inlineSchemaHash": "375af4f267115d3abf5efd636fefa9d71336e214684e9eaaf0c05cc09e16251b",
+  "inlineSchema": "// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\ngenerator client {\n  provider      = \"prisma-client\"\n  output        = \"../src/model/prisma\"\n  binaryTargets = [\"native\", \"rhel-openssl-3.0.x\"]\n}\n\ndatasource db {\n  provider = \"sqlite\"\n  // url      = env(\"DATABASE_URL\")\n  url      = \"file:dev.db\"\n}\n\nmodel Profile {\n  id   Int     @id @default(autoincrement())\n  name String?\n}\n",
+  "inlineSchemaHash": "189080311ab83e903944dc218304e5fbe888d825f66a1cb9045320922ceab1e1",
   "copyEngine": true,
   "runtimeDataModel": {
     "models": {},
