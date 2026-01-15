a564c0e36bca64a29b46cd4942e9ce37ef8fb870
Ajmir Raziqi <ajmir.ng3@gmail.com>
2026-01-15 19:14:43 +0200

    create a bottom nav dock

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,72 +6,44 @@ import {
   SettingsIcon,
   UserIcon,
 } from "lucide-react";
+import type { ReactNode } from "react";
 import { NavLink } from "react-router";
+
+const DLink = ({
+  icon,
+  label,
+  to,
+  className,
+}: {
+  icon: ReactNode;
+  to: string;
+  label: string;
+  className?: string;
+}) => (
+  <NavLink
+    to={to}
+    className={({ isActive }) =>
+      cn(
+        "flex justify-center items-center gap-2 hover:bg-primary duration-300  p-2 sm:px-3 rounded-xl",
+        isActive && "bg-primary/80",
+        className
+      )
+    }
+  >
+    {icon}
+    <span className="hidden sm:inline uppercase">{label}</span>
+  </NavLink>
+);
 
 export default function Navbar() {
   return (
-    <div className="w-full fixed bottom-4 left-0 flex justify-center">
-      <div className="bg-secondary/50 backdrop-blur-lg p-2 rounded-2xl flex items-center gap-2 justify-center">
-        <NavLink
-          to={"/"}
-          className={({ isActive }) =>
-            cn(
-              "flex justify-center items-center gap-2 hover:bg-primary duration-300 p-4 rounded-xl",
-              isActive && "bg-primary/50"
-            )
-          }
-        >
-          <HomeIcon />
-          <span className="hidden sm:inline">Home</span>
-        </NavLink>
-        <NavLink
-          to={"/search"}
-          className={({ isActive }) =>
-            cn(
-              "flex justify-center items-center gap-2 hover:bg-primary duration-300 p-4 rounded-xl",
-              isActive && "bg-primary/50"
-            )
-          }
-        >
-          <SearchIcon />
-          <span className="hidden sm:inline">Search</span>
-        </NavLink>
-        <NavLink
-          to={"/add"}
-          className={({ isActive }) =>
-            cn(
-              "flex justify-center items-center gap-2 hover:bg-primary duration-300 p-4 rounded-xl",
-              isActive && "bg-primary/50"
-            )
-          }
-        >
-          <PlusIcon />
-          <span className="hidden sm:inline">Add</span>
-        </NavLink>
-        <NavLink
-          to={"/profile"}
-          className={({ isActive }) =>
-            cn(
-              "flex justify-center items-center gap-2 hover:bg-primary duration-300 p-4 rounded-xl",
-              isActive && "bg-primary/50"
-            )
-          }
-        >
-          <UserIcon />
-          <span className="hidden sm:inline">Profile</span>
-        </NavLink>
-        <NavLink
-          to={"/settings"}
-          className={({ isActive }) =>
-            cn(
-              "flex justify-center items-center gap-2 hover:bg-primary duration-300 p-4 rounded-xl",
-              isActive && "bg-primary/50"
-            )
-          }
-        >
-          <SettingsIcon />
-          <span className="hidden sm:inline">Settings</span>
-        </NavLink>
+    <div className="w-full fixed bottom-0 left-0 flex justify-center">
+      <div className="bg-foreground/10 backdrop-blur-lg p-2 rounded-t-2xl flex items-center gap-2 justify-center">
+        <DLink to={"/"} icon={<HomeIcon />} label="Home" />
+        <DLink to={"/search"} icon={<SearchIcon />} label="Search" />
+        <DLink to={"/add"} icon={<PlusIcon />} label="Add" />
+        <DLink to={"/profile"} icon={<UserIcon />} label="Profile" />
+        <DLink to={"/settings"} icon={<SettingsIcon />} label="Settings" />
       </div>
     </div>
   );
